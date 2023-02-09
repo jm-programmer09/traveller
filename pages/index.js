eef2985ba29be7b280c0a2e9aaf0041d0f55f52b
jm-programmer09 <jackmellett2009@gmail.com>
2023-02-09 21:34:56 +1000

    realised i wont use the database

--- pages/index.js
+++ pages/index.js
@@ -11,19 +11,16 @@ import fourth from "@/public/background2.jpg";
 
 // This is for the firebase (aka the database)
 // These are the settings to connect (for authorisation). The process.env is found in the .env.local file
-const dbconfig = {
-  apiKey: process.env.KEY,
-  authDomain: process.env.DOMAIN,
-  projectId: process.env.PROJECT,
-  storageBucket: process.env.STORAGE,
-  messagingSenderId: process.env.SENDERID,
-  appId: process.env.SENDERID,
-  measurementId: process.env.MEASURE
-};
-
 // TODO
 // If u really want, make it so that the slider is animated
 
+
+function TravelElement(){
+  return (
+    <>
+    </>
+  );
+};
 // THis is teh main function
 export default function Home() {
   // This is setting the useState variables that will allow for me to change the classList and make it so that the photos show and hide
@@ -48,10 +45,12 @@ export default function Home() {
       if (image_main+1 == max_image) { image_main = 1;}
       else if (image_main < max_image) { image_main++;} 
     }, image_change_timing);
-
   }, []);
   /////////////////////////////////////////////////////
-
+  // This is for loading the database
+  useEffect(() => {
+    
+  }, []);
 
 
   // THis is the main return that renders the HTML
